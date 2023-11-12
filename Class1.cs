751b4e73f5676cea4929c4ca768365f2f26f6b56
We8eR <122805348+We8eR@users.noreply.github.com>
2023-11-12 16:18:44 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+public static class Program
+{
+    public static void Main()
+    {
+        foreach(EncodingInfo ei in Encoding.GetEncodings())
+        {
+            Encoding e = ei.GetEncoding();
+            Console.WriteLine("{1}{0}" + "\tCodePage={2}, WindowsCodePage={3}{0}" + "\tWebName={4}, HeaderName={5}, BodyName={6}{0}" + "\tIsBrowserDisplay={7}, IsBrowserSave={8}{0}" + "\tIsMailNewsDisplay={9}, IsMailNewsSave={10}{0}", 
+                Environment.NewLine,
+                e.EncodingName, e.CodePage, e,WindowsCodePage,
+                e.WebName, e.HeaderName, e.BodyName,
+                e.IsBrowserDisplay, e.IsBrowserSave,
+                e.IsMailNewsDisplay, e.IsMailNewsSave);
+        }
+    }
+}
